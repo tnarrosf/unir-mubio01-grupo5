4ee14dd452312f1fbbc6507749a0b7400bc737cc
Tomás Narros <tnarrosf@gmail.com>
2024-04-28 18:45:47 +0200

    Tabla de tipo de estudio

---
+++ basesdatos/sql-scripts/actividadgrupal_dml.sql
@@ -0,0 +1,30 @@
+INSERT INTO `actividadgrupal_new`.`tipo_secuencia` (`descripcion`)
+VALUES
+('Exón'),
+('Intrón'),
+('ARNm'),
+('miARN'),
+('snARN'),
+('snoARN'),
+('tARN'),
+('rARN'),
+('Promotor'),
+('Terminador'),
+('Enhancer'),
+('Silencer');
+
+
+INSERT INTO `actividadgrupal_new`.`tipo_variante` (`descripcion`)
+VALUES
+('exon'),
+('intron'),
+('ARNm'),
+('miARN'),
+('snARN'),
+('snoARN'),
+('tARN'),
+('rARN'),
+('promotor'),
+('terminador'),
+('enhancer'),
+('silencer');
